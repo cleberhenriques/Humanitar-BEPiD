42a051f5178eb93be4f5272ffe3e0d90e0a7b199
Stephan Chang <stephanchang@Stephans-MacBook-Pro.local>
2013-12-11 08:22:01 -0200

    Creating base structure

---
+++ VoosBesouros/VoosBesouros/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VoosBesouros
+//
+//  Created by Stephan Chang on 12/11/13.
+//  Copyright (c) 2013 Stephan Chang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
