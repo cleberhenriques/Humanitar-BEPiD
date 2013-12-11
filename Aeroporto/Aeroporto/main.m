fc579699cc27b86c02897cc74510ec555b8db002
Felipe Delazeri Riffel <FRiffel@MacBook-Pro-de-Felipe-2.local>
2013-12-11 08:16:04 -0200

    proj

---
+++ Aeroporto/Aeroporto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Aeroporto
+//
+//  Created by Felipe Delazeri Riffel on 11/12/13.
+//  Copyright (c) 2013 Felipe D. Riffel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VBAppDelegate class]));
+    }
+}
