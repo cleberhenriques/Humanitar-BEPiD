0f8821a29943fc90a42d45e768297d9080fce9ff
Leonardo Gubert <leonardogubert@MacBook-Pro-de-Leonardo.local>
2013-12-11 09:34:23 -0200

    Flights

---
+++ Vôos Bizarros/Vôos Bizarros/VBFlight.h
@@ -0,0 +1,22 @@
+//
+//  VBFlight.h
+//  Vôos Bizarros
+//
+//  Created by Leonardo Gubert on 11/12/13.
+//  Copyright (c) 2013 com.TDW.app. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VBFlight : NSObject
+
+
+//@property NSMutableArray *categories;
+
+@property NSString *flID;
+
+-(NSString*)getCategories;
+-(BOOL)addCategorie:(NSString*)categorie;
+
+	
+@end
