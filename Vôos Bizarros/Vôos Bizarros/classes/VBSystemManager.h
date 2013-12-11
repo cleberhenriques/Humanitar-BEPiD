cca8aaa7cd219e1b55ade7bde856e9b01617124d
Felipe Delazeri Riffel <FRiffel@MacBook-Pro-de-Felipe-2.local>
2013-12-11 10:05:10 -0200

    classe SystemManager

---
+++ Vôos Bizarros/Vôos Bizarros/classes/VBSystemManager.h
@@ -0,0 +1,13 @@
+//
+//  VBSystemManager.h
+//  Vôos Bizarros
+//
+//  Created by Felipe Delazeri Riffel on 11/12/13.
+//  Copyright (c) 2013 com.TDW.app. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VBSystemManager : NSObject
+
+@end
