b4111b51832048523af0cef57f303a16e38c8512
Leonardo Gubert <leonardogubert@MacBook-Pro-de-Leonardo.local>
2013-12-11 10:59:08 -0200

    Metodo create flight

--- Vôos Bizarros/Vôos Bizarros/classes/VBSystemManager.m
+++ Vôos Bizarros/Vôos Bizarros/classes/VBSystemManager.m
@@ -21,8 +21,30 @@
 
 -(void)createFlight:(NSString *)name :(NSString *)origem :(NSString *)destino :(int)year :(int)month :(int)day :(NSString *)flightID{
 	
-
+	VBAirline *air = [self findCompany:name];
+	if(air == nil){
+		NSLog(@"Companhia nao existente.");
+	}
 	
+	VBFlight *voo = [[VBFlight alloc] init];
+	voo.orig = origem;
+	voo.dest = destino;
+	voo.year = year;
+	voo.month = month;
+	voo.day = day;
+	voo.flID = flightID;
+	
+	[air addFlight: voo];
 }
+
+-(VBAirline*)findCompany:(NSString*)name{
+	for (VBAirline* comp in airlines) {
+		if([name compare: comp.nome]){
+			return comp;
+		}
+	}
+	return nil;
+}
+
 
 @end
