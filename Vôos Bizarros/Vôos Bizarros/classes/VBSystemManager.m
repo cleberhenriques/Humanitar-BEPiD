94751925957821251f579298dc305e28baa91aa5
Leonardo Gubert <leonardogubert@MacBook-Pro-de-Leonardo.local>
2013-12-11 11:05:17 -0200

    vai sabe

--- Vôos Bizarros/Vôos Bizarros/classes/VBSystemManager.m
+++ Vôos Bizarros/Vôos Bizarros/classes/VBSystemManager.m
@@ -14,6 +14,54 @@
     NSMutableArray *airlines;
 }
 
+-(instancetype)init
+{
+    if (self = [super init])
+    {
+        airports = [[NSMutableArray alloc]init];
+        airlines = [[NSMutableArray alloc]init];
+    }
+    return self;
+}
+
+-(BOOL)createAirline:(NSString*) name
+{
+    if([name length] <= 5)
+    {
+        VBAirline* airline = [[VBAirline alloc] init];
+        for(VBAirline* al in airlines)
+        {
+			if([airline.nome isEqual:name])
+				return NO;
+        }
+        
+        airline.nome = name;
+        [airlines addObject:airline];
+        return YES;
+    }
+    
+    return NO;
+}
+
+-(void)createAirport:(NSString *)airportName
+{
+    VBAirport *airport = [[VBAirport alloc]initWithName:airportName];
+    [airports addObject:airport];
+}
+
+-(void)bookSeat:(NSString *)air flight:(NSString *)fl seatClass:(VBSeatClass)s row:(int)row col:(char)col
+{
+    
+}
+
+- (NSArray *) findAvailableFlights: (NSString *) origin : (NSString *) destination
+{
+    
+    
+    return nil;
+}
+
+
 -(void)createFlight:(NSString *)name :(NSString *)origem :(NSString *)destino :(int)year :(int)month :(int)day :(NSString *)flightID{
 	
 	VBAirline *air = [self findCompany:name];
