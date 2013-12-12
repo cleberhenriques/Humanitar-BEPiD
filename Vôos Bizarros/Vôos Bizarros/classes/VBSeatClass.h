9f81ae728a7ce68fc505df5352754efb0f268c37
Felipe Delazeri Riffel <FRiffel@MacBook-Pro-de-Felipe-2.local>
2013-12-12 10:39:20 -0200

    teste

---
+++ Vôos Bizarros/Vôos Bizarros/classes/VBSeatClass.h
@@ -0,0 +1,15 @@
+//
+//  VBSeatClass.h
+//  Vôos Bizarros
+//
+//  Created by Txai Wieser on 11/12/13.
+//  Copyright (c) 2013 com.TDW.app. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM (NSInteger, VBSeatClass) {
+    Primeira,
+    Executiva,
+    Economica
+};
