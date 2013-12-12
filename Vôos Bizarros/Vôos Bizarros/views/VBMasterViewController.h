6a6724c0fbc79c4916df4e5d4511cb8f33e29d2b
Txai Wieser <txaidw@gmail.com>
2013-12-12 13:50:08 -0200

    add view details

---
+++ Vôos Bizarros/Vôos Bizarros/views/VBMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  VBMasterViewController.h
+//  k
+//
+//  Created by Txai Wieser on 12/12/13.
+//  Copyright (c) 2013 com.TDW.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class VBDetailViewController;
+
+@interface VBMasterViewController : UITableViewController
+
+@property (strong, nonatomic) VBDetailViewController *detailViewController;
+
+@end
