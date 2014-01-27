e085cb44ea33c96657987d4532624658e2f679bc
Cleber Henriques <cleberhenriques@gmail.com>
2014-01-27 11:36:53 -0200

    Adicionando parse frameworki. Começando tela de login

---
+++ ComunitarLogin/ComunitarLogin/CHViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHViewController.h
+//  ComunitarLogin
+//
+//  Created by Cleber Henriques on 27/01/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHViewController : UIViewController
+
+@end
---
+++ ComunitarLogin/ComunitarLogin/CHViewController.m
@@ -0,0 +1,29 @@
+//
+//  CHViewController.m
+//  ComunitarLogin
+//
+//  Created by Cleber Henriques on 27/01/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "CHViewController.h"
+
+@interface CHViewController ()
+
+@end
+
+@implementation CHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
