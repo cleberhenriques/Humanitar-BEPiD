aa1770982daa0a0a7b082d0b2f507d6922008fe5
Txai Wieser <txaidw@gmail.com>
2014-01-28 22:41:39 -0200

    now using Event class

---
+++ PROJ1TableView/PROJ1TableView/PROJ1Evento.m
@@ -0,0 +1,13 @@
+//
+//  PROJ1Evento.m
+//  PROJ1TableView
+//
+//  Created by Txai Wieser on 1/28/14.
+//  Copyright (c) 2014 com.TDW.app. All rights reserved.
+//
+
+#import "PROJ1Evento.h"
+
+@implementation PROJ1Evento
+
+@end
