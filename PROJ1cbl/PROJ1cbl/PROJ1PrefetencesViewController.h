29e8de90a6ab4a4c118f2ba7253f55594d3f03c9
Txai Wieser <txaidw@gmail.com>
2014-02-04 10:34:17 -0200

    new View Preferences

---
+++ PROJ1cbl/PROJ1cbl/PROJ1PrefetencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PROJ1PrefetencesViewController.h
+//  PROJ1cbl
+//
+//  Created by Txai Wieser on 04/02/14.
+//  Copyright (c) 2014 com.TDW.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PROJ1PrefetencesViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ PROJ1cbl/PROJ1cbl/PROJ1PrefetencesViewController.m
@@ -0,0 +1,38 @@
+//
+//  PROJ1PrefetencesViewController.m
+//  PROJ1cbl
+//
+//  Created by Txai Wieser on 04/02/14.
+//  Copyright (c) 2014 com.TDW.app. All rights reserved.
+//
+
+#import "PROJ1PrefetencesViewController.h"
+
+@interface PROJ1PrefetencesViewController ()
+
+@end
+
+@implementation PROJ1PrefetencesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
