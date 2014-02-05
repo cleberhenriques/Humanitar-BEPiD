6a81dd3f6f320d52690bab7208bf908b7f6f7413
Cleber Henriques <cleberhenriques@gmail.com>
2014-02-05 10:20:18 -0200

    w

---
+++ PROJ1cbl/PROJ1cbl/PROJ1UserInfoSingleton.m
@@ -0,0 +1,30 @@
+//
+//  PROJ1UserInfoSingleton.m
+//  PROJ1cbl
+//
+//  Created by Cleber Henriques on 05/02/14.
+//  Copyright (c) 2014 com.TDW.app. All rights reserved.
+//
+
+#import "PROJ1UserInfoSingleton.h"
+
+@implementation PROJ1UserInfoSingleton
+
++ (id)sharedManager {
+    static PROJ1UserInfoSingleton *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+- (id)init{
+    NSLog(@"setting nome");
+    if (self = [super init]) {
+        _nome = @"Default Property Value";
+    }
+    return self;
+}
+
+@end
