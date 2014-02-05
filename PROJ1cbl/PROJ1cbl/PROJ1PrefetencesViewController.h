ed031b052033fc3bb274b00f2719844341eaa866
Txai Wieser <txaidw@gmail.com>
2014-02-05 00:15:36 -0200

    preferences

--- PROJ1cbl/PROJ1cbl/PROJ1PrefetencesViewController.h
+++ PROJ1cbl/PROJ1cbl/PROJ1PrefetencesViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PROJ1PrefetencesViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface PROJ1PrefetencesViewController : UIViewController
 
 @end
