cf6f26aaa5c6fcbe07a39a03fd06f022268187ff
fabiocampero <fabiocampero@gmail.com>
2014-02-07 11:21:43 -0200

    Classes parse

--- PROJ1cbl/PROJ1cbl/PROJ1ProfileViewController.m
+++ PROJ1cbl/PROJ1cbl/PROJ1ProfileViewController.m
@@ -8,6 +8,7 @@
 
 #import "PROJ1ProfileViewController.h"
 #import "PROJ1Usuario.h"
+#import <Parse/Parse.h>
 
 @interface PROJ1ProfileViewController ()
 
@@ -34,6 +35,71 @@
     [self.instutuicoesMaisVisitadasProfile setDelegate:self];
     [self.instutuicoesMaisVisitadasProfile setDataSource:self];
 	// Do any additional setup after loading the view.
+    
+    
+    NSLog(@"TESTE OK");
+    
+    // salvar
+    PFObject *gameScore = [PFObject objectWithClassName:@"Evento"];
+    gameScore[@"titulo"] = @"Festa no AP";
+    [gameScore saveInBackground];
+    
+    
+    // listar um registro
+    PFQuery *query = [PFQuery queryWithClassName:@"Evento"];
+    [query getObjectInBackgroundWithId:@"J3RIqZATIs" block:^(PFObject *gameScore, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        NSLog(@"%@", gameScore[@"titulo"]);
+    }];
+    
+    
+    // Editar
+    PFQuery *editar = [PFQuery queryWithClassName:@"Evento"];
+    
+    // Retrieve the object by id
+    [editar getObjectInBackgroundWithId:@"MA52ndkeWH" block:^(PFObject *retorno, NSError *error) {
+        
+        retorno[@"titulo"] = @"Não festa no AP";
+        [retorno saveInBackground];
+        
+    }];
+    
+    
+    // deletar
+    PFQuery *deletar = [PFQuery queryWithClassName:@"Evento"];
+    [deletar getObjectInBackgroundWithId:@"OUqbxoelsI" block:^(PFObject *retorno, NSError *error) {
+        
+        [retorno deleteInBackground];
+        [retorno saveInBackground];
+        
+    }];
+    
+    
+    // listar vários dados
+    PFQuery *listar = [PFQuery queryWithClassName:@"Evento"];
+    
+    // Retrieve the most recent ones
+    [listar orderByDescending:@"createdAt"];
+    
+    // Only retrieve the last ten
+    listar.limit = 10;
+    
+    // Include the post data with each comment
+    [listar includeKey:@"post"];
+    
+    [listar findObjectsInBackgroundWithBlock:^(NSArray *result, NSError *error) {
+        // Comments now contains the last ten comments, and the "post" field
+        // has been populated. For example:
+        for (PFObject *resultado in result) {
+            // This does not require a network access.
+            PFObject *post = resultado[@"titulo"];
+            NSLog(@"%@", post);
+        }
+    }];
+    
+
+    
+    
 }
  -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
